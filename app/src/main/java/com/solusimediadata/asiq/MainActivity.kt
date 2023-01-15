982e75ced9b8984bef554195dbd3c8b60a287b4f
Andre Reynaldi Lusikooy <reynaldi@outlook.jp>
2023-01-15 23:51:51 +0700

    init theme, upgrade sdk

--- app/src/main/java/com/solusimediadata/asiq/MainActivity.kt
+++ app/src/main/java/com/solusimediadata/asiq/MainActivity.kt
@@ -1,11 +1,56 @@
 package com.solusimediadata.asiq
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
+import com.solusimediadata.asiq.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedListener {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    lateinit var appBarConfiguration: AppBarConfiguration
+    private val topLevelFragments = setOf(
+        R.id.loginFragment,
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation() {
+        binding.bottomNavigation.itemIconTintList = null
+        navController = findNavController(R.id.navHostFragment)
+        navController.addOnDestinationChangedListener(this)
+        appBarConfiguration = AppBarConfiguration(topLevelFragments)
+        binding.bottomNavigation.setupWithNavController(navController)
+    }
+
+    override fun onDestinationChanged(
+        controller: NavController,
+        destination: NavDestination,
+        arguments: Bundle?
+    ) {
+        when (destination.id) {
+            R.id.loginFragment->{
+                binding.bottomNavigation.visibility = View.GONE
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
